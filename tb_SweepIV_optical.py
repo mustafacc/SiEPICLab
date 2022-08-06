ac900181469cd680e4666cd52da89e2a1f5a21d2
Mustafa <mustafa@ece.ubc.ca>
2022-08-05 22:50:49 -0700

    Add SweepIV_optical sequence

---
+++ tb_SweepIV_optical.py
@@ -0,0 +1,32 @@
+"""
+SiEPIClab testbench.
+
+IV Sweep for the Keithley class source measure unit class.
+
+Mustafa Hammood, SiEPIC Kits, 2022
+"""
+# %%
+import pyvisa as visa
+import numpy as np
+from siepiclab.sequences.SweepIV_optical import SweepIV_optical
+from siepiclab.drivers.smu_keithley import smu_keithley
+from siepiclab.drivers.PowerMonitor_keysight import PowerMonitor_keysight
+rm = visa.ResourceManager()
+
+# %% instruments definition
+smu = smu_keithley(rm.open_resource('keithley_2602'))
+pm1 = PowerMonitor_keysight(rm.open_resource('mainframe_1550'), chan='1', slot='1')
+pm2 = PowerMonitor_keysight(rm.open_resource('mainframe_1550'), chan='1', slot='2')
+# %% routine definition
+v_min = 0
+v_max = 1
+v_res = 0.05
+
+sequence = SweepIV_optical(smu, [pm1, pm2])
+sequence.v_pts = np.arange(v_min, v_max, v_res)
+sequence.chan = 'B'
+sequence.verbose = True
+sequence.visual = True
+sequence.execute()
+
+sequence.results.save()
