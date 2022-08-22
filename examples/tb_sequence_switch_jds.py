0fccc01e9520ffa690f2e28ca100c0f8ee3f888c
Davin Birdi <davin.birdi@alumni.ubc.ca>
2022-08-22 16:59:17 -0700

    Append to instruments instead of setting instruments

--- examples/tb_sequence_switch_jds.py
+++ examples/tb_sequence_switch_jds.py
@@ -20,7 +20,7 @@ print(rm.list_resources())
 # %% instruments definition
 
 switch_gpib = 'GPIB0::7::INSTR'
-switch_gpib = 'visa://10.2.137.163/GPIB0::7::INSTR'
+#switch_gpib = 'visa://10.2.137.163/GPIB0::7::INSTR'
 jds = opticalSwitch_jds(rm.open_resource(switch_gpib), chan='')
 
 
