d9a0b3b56f6ee1b021382aac0f3cabea2ffe67ed
Davin Birdi <davin.birdi@alumni.ubc.ca>
2022-08-31 17:25:21 -0700

    [milestone] Successfully running sweeps through each switch input with results saved to parent call

--- examples/tb_sequence_LabCartSwitchMeasurement.py
+++ examples/tb_sequence_LabCartSwitchMeasurement.py
@@ -14,12 +14,14 @@ from measurements import sequence
 
 from siepiclab.drivers.opticalSwitch_jds import opticalSwitch_jds
 from siepiclab.drivers.PolCtrl_keysight import PolCtrl_keysight
-from siepiclab.drivers.fls_keysight import fls_keysight
+from siepiclab.drivers.tls_keysight import tls_keysight
 from siepiclab.drivers.PowerMonitor_keysight import PowerMonitor_keysight
+from siepiclab.drivers.lwmm_keysight import lwmm_keysight
 
 from siepiclab.sequences.SwitchPath import SwitchPath
 from siepiclab.sequences.SweepPolarization import SweepPolarization
 from siepiclab.sequences.SweepPolarization_SwitchPaths import SweepPolarization_SwitchPaths
+from siepiclab.sequences.SwitchSequences import SwitchSequences
 
 
 import numpy as np
@@ -32,20 +34,21 @@ print(rm.list_resources())
 
 if True:
     switch_gpib = 'GPIB0::7::INSTR'
-    fls_gpib = 'GPIB0::20::INSTR'
+    mf_gpib = 'GPIB0::20::INSTR'
     pol_gpib = 'GPIB0::8::INSTR'
 else:
     switch_gpib = 'visa://10.2.137.163/GPIB0::7::INSTR'
-    fls_gpib = 'visa://10.2.137.163/GPIB0::20::INSTR'
+    mf_gpib = 'visa://10.2.137.163/GPIB0::20::INSTR'
     pol_gpib = 'visa://10.2.137.163/GPIB0::8::INSTR'
 
 
 jds = opticalSwitch_jds(rm.open_resource(switch_gpib), chan='')
-fls = fls_keysight(rm.open_resource(fls_gpib), chan='2')
-pol = PolCtrl_keysight(rm.open_resource(pol_gpib), chan='')
-pm = PowerMonitor_keysight(rm.open_resource(fls_gpib), chan='1', slot=1)
+tls = tls_keysight(rm.open_resource(mf_gpib), chan='2')
+#pol = PolCtrl_keysight(rm.open_resource(pol_gpib), chan='')
+pm = PowerMonitor_keysight(rm.open_resource(mf_gpib), chan='1', slot=1)
+mf = lwmm_keysight(rm.open_resource(mf_gpib))
 
-
+"""
 calibration = SweepPolarization_SwitchPaths(fls, pol, pm, jds)
 
 calibration.scantime = 5
@@ -76,6 +79,15 @@ sequence.results.add('lot_id', lot_id)
 calibration.results.save(calibration.file_name)
 #sequence.results.save(f"{chip_id}_{die_id}_{device_id}")
 
+"""
+file_name = '2022-08-31-1524_NoPolTest_7-8'
+
+sequence = SwitchSequences(tls, pm, mf, jds, verbose=True, saveplot=True, visual=True)
+#sequence = SwitchSequences(tls, pm, pol, mf, jds, verbose=True, saveplot=True, visual=True)
+sequence.file_name = file_name
+
+sequence.range = [7,8]
+sequence.execute()
 
 
 
---
+++ siepiclab/sequences/SwitchSequences/__init__.py
@@ -0,0 +1,120 @@
+"""
+SiEPIClab measurement sequence.
+
+Switch sweep sequence. While toggling through a range, run multiple experiments via different optical paths.
+
+Davin Birdi, UBC Photonics
+
+With Templates from:
+Mustafa Hammood, SiEPIC Kits, 2022
+"""
+from SweepPolarization import SweepPolarization
+from SweepWavelengthSpectrum import SweepWavelengthSpectrum
+from SwitchPath import SwitchPath
+from siepiclab import measurements
+
+
+class SwitchSequences(measurements.sequence): 
+    def __init__(self, tls, pm, mf, sw,  verbose=False, visual=False, saveplot=False):
+#    def __init__(self, tls, pm, pol, mf, sw,  verbose=False, visual=False, saveplot=False):
+        super().__init__(self)
+
+        #SweepWavelengthSpectrum.__init__(self, mf, tls, pm, mode='step')
+
+
+        self.tls = tls
+        self.pm = pm
+#        self.pol = pol
+        self.mf = mf
+        self.jds = sw
+
+        self.instruments.extend([tls, pm, mf, sw])
+
+        self.range = [1,2]
+        
+        # Initialize all the Options. This can be done better with properties and cycling through a list/dictionary        
+        self.verbose = verbose
+        self.visual = visual
+        self.saveplot = saveplot
+
+
+        self.Polarization = False
+        if self.Polarization:
+            self.seq_polopt = SweepPolarization(tls, pol, pm)
+            self.seq_polopt.optimize = True
+            self.seq_polopt.verbose = self.verbose
+            self.seq_polopt.visual = self.visual
+            self.seq_polopt.saveplot = self.saveplot
+            self.seq_polopt.reset_after_execution = False
+
+        self.WLSweep = True
+        if self.WLSweep:
+            self.seq_wlsweep = SweepWavelengthSpectrum(mf, tls, pm, mode='step')
+            self.seq_wlsweep.verbose = self.verbose  # turn on verbose logging mode
+            self.seq_wlsweep.visual = self.visual  # visualize the wavelength sweep results
+            self.seq_wlsweep.saveplot = self.saveplot
+    
+            
+
+        self.experiment = measurements.lab_setup(self.instruments)
+
+    def InstrSetting(self):
+        pass
+
+    def instructions(self):
+        
+        """Instructions of the sequence."""
+        
+        if self.verbose:
+            print("\n***Sequence Starting...***")
+
+
+        rslts_maxT = []
+        rslts_minT = []
+
+        rslts_wavl = []
+        rslts_pwr = []
+
+        file_name = self.file_name
+
+        for i in self.range:
+            
+            self.chan = i
+            self.file_name = str(file_name) + '-chan' + str(self.chan)
+            if self.verbose:
+                print("\n***Switch Changing...***")
+            SwitchPath.instructions(self)
+            
+            if self.Polarization:
+                if self.verbose:
+                    print("\n***Optimizing Polarization...***")
+                self.seq_polopt.execute()
+            
+                maxT = self.seq_polopt.results.data['maxT']
+                minT = self.seq_polopt.results.data['minT']
+                rslts_maxT.append(maxT)
+                rslts_minT.append(minT)
+
+            if self.WLSweep:
+                self.seq_wlsweep.file_name = self.file_name
+                self.seq_wlsweep.execute()
+                wavl = self.seq_wlsweep.results.data['rslts_wavl']
+                pwr  = self.seq_wlsweep.results.data['rslts_pwr']
+                self.results.add('wlsweep' + '-chan' + str(self.chan), [ wavl, pwr  ])
+
+            pass
+
+        self.file_name = file_name
+        self.results.add('rslts_maxT', rslts_maxT)
+        self.results.add('rslts_minT', rslts_minT)
+
+        
+            
+
+
+        
+        
+        
+        
+
+    
