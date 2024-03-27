e4912225fb20586b0294d985ab4c13a31c952e77
Davin Birdi <davin.birdi@dreamphotonics.com>
2024-03-27 15:02:23 -0700

    Add LDC+SMU Seq. & Resolve Bugs in SMU Driver

--- siepiclab/sequences/onchipLDtoPD/__init__.py
+++ siepiclab/sequences/onchipLDtoPD/__init__.py
@@ -1,3 +1,4 @@
+# %%
 """
 SiEPIClab measurement sequence.
 
@@ -28,18 +29,16 @@ class onchipLDtoPD(measurements.sequence):
     def __init__(self, ldc, smu):
         super(onchipLDtoPD, self).__init__()
 
-        #self.ldc = ldc
-        #self.smu = smu
-        self.ldc = ldc_srs_ldc500()
-        self.smu = smu_keithley2400()
+        self.ldc = ldc
+        self.smu = smu
 
-
+        # Default Settings: LDC
         self.Imin = 0
-        self.Imax = 10
-        self.numPts = 2 + 1
-
-        # Default Settings:
-        self.v_bias = -1              # Volts:
+        self.Imax = 20
+        self.numPts = 5 + 1
+        
+        # Default Settings: SMU
+        self.v_bias = -1            # Volts:
         self.i_compliance = 10e-3   # Amps:
 
         # Lab Setup:
@@ -48,7 +47,12 @@ class onchipLDtoPD(measurements.sequence):
 
     def InstrSetting(self):
     
+        smu.reset()
+
         # Set the Reverse Bias Voltage and the Compliance Current:
+        self.smu.SetCurrentMode()
+
+        #self.smu.
         self.smu.SetVoltage(self.v_bias)
         self.smu.SetCurrentLimit(self.i_compliance)
 
@@ -70,11 +74,19 @@ class onchipLDtoPD(measurements.sequence):
         
         ld_currents=[]
         ld_voltages=[]
-    
 
+        pd_currents=[]
+        pd_voltages=[]
+    
+        # Turn On Equipment:
         if self.verbose:
             print('***Activating TEC...***')
         self.ldc.tecON()
+        
+        if self.verbose:
+            print('***Activating SMU...***')
+        self.smu.SetOutput(True)
+        
         if self.verbose:
             print('***Activating LD and Beginning Current Sweep...***')
         self.ldc.LDON()
@@ -82,34 +94,97 @@ class onchipLDtoPD(measurements.sequence):
 
         for ii, II in enumerate(currentsSet):
             self.ldc.SetLDcurrent(II)
-            sleep(2)
+            sleep(1)
             pd_currents = np.append(pd_currents, self.smu.GetCurrent())
-            pd_voltages = np.append(pd_voltages, self.smu.GetCurrent())
+            pd_voltages = np.append(pd_voltages, self.v_bias)
             
             ld_currents = np.append(ld_currents,self.ldc.GetLDcurrent())
             ld_voltages = np.append(ld_voltages,self.ldc.GetLDvoltage())
                 
         if self.verbose:
-            print('***Turning Off LD:***')
+            print('***Turning Off:***')
         self.ldc.SetLDcurrent(0)
         self.ldc.LDOFF()
+        self.smu.SetOutput(False)
+
+        sweepdata = measurements.results()
 
 
-        self.results.add('pd_currents', pd_currents)
-        self.results.add('pd_voltages', pd_voltages)
-        self.results.add('ld_currents', ld_currents)
-        self.results.add('ld_voltages', ld_voltages)
+        sweepdata.add('pd_currents', pd_currents)
+        sweepdata.add('pd_voltages', pd_voltages)
 
+        sweepdata.add('ld_currents', ld_currents)
+        sweepdata.add('ld_voltages', ld_voltages)
 
+        self.results.add('sweepdata', sweepdata)
 
-if __name__ == '__main___':
+print('Hello World')
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    import pandas as pd
+
+    from datetime import datetime
+
     rm = visa.ResourceManager()
 
 
-    ldc = ldc_srs_ldc500(rm.open_resource('GPIB0::20::INSTR'))
-    smu = smu_keithley2400(rm.open_resource('GPIB0::20::INSTR'))
+    ldc = ldc_srs_ldc500(rm.open_resource('ldc'))
+    smu = smu_keithley2400(rm.open_resource('smu'))
 
     print(smu.identify())
     print(ldc.identify())
     
+
+    
+    
+    
+    
     sequence = onchipLDtoPD(ldc, smu)
+    sequence.verbose = True
+
+    # Setup the Sequence:
+    test = False
+    if test:
+        # Sanity Check
+        sequence.Imin = 0
+        sequence.Imax = 10
+        sequence.numPts = 3 + 1
+
+    else:
+        #Normal Settings
+        sequence.Imin = 0
+        sequence.Imax = 50
+        sequence.numPts = 200 + 1
+
+    sequence.v_bias = 1             # Volts:
+    sequence.i_compliance = 10e-3   # Amps:
+
+    sequence.execute()
+    
+    date = datetime.now().strftime("%y-%m-%d_%H-%M-%S")
+    experiment_name = 'LDC-CurrentSweep_SMU-VoltageBias'
+    
+    measID = f'ARL_onchipIntegration_Sweep-{sequence.Imin}-{sequence.Imax}mA_Bias-{sequence.v_bias}V'
+    basedir = 'C:\\Users\\dream\\OneDrive\\Documents\\dp_experiments\\ARL_onChipIntegration'
+    savedir = basedir + '\\' + measID + '\\' + date + '\\'
+    
+    filename = savedir + measID
+    sequence.results.createDir(savedir)
+
+    measID = measID + '_hires'
+
+    # Get the Data from the Sweep:
+    sweepdata = sequence.results.data['sweepdata'].data
+
+    # Save to CSV
+    df = pd.DataFrame(sweepdata)    
+    df.to_csv(filename + '.csv')
+    
+    # Plot and save as PNG
+    fig, ax = plt.subplots(1,1)
+    ax.plot(sweepdata['ld_currents'], 1e3*sweepdata['pd_currents'])
+    ax.set_ylabel("PD Current mA")
+    ax.set_xlabel("LD Current mA")
+    fig.savefig(filename + '_plot.png')
+# %%
